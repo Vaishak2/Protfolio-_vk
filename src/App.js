558f0916d1fe755b743a861e2b76872ba10135c9
Vaishak M <vaishakvaishak3@gmail.com>
2025-10-11 15:10:30 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <ul id="gallery-filters_gal">
+            
+        <li data-filter_gal=".filter_gal-bike"class="filter_gal-active">BUSINESS PROFILE</li>
+        <li data-filter_gal=".filter_gal-personal">PERSONAL PROFILE</li>
+        <li data-filter=".filter-card">GRAPHIC DESIGNS</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
